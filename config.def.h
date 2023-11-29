e78a1a4e79f1f952be577c628e1f0421651625fa
Mitch Goddard <mitch@mitchdotnet.net>
2023-11-29 13:47:10 -0500

    Gaps tweak for perfect terminal borders

--- config.def.h
+++ config.def.h
@@ -6,10 +6,10 @@
 static const unsigned int borderpx       = 0;   /* border pixel of windows */
 static const unsigned int snap           = 32;  /* snap pixel */
 static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
-static const unsigned int gappih         = 35;  /* horiz inner gap between windows */
-static const unsigned int gappiv         = 35;  /* vert inner gap between windows */
-static const unsigned int gappoh         = 35;  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov         = 35;  /* vert outer gap between windows and screen edge */
+static const unsigned int gappih         = 37;  /* horiz inner gap between windows */
+static const unsigned int gappiv         = 37;  /* vert inner gap between windows */
+static const unsigned int gappoh         = 37;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 37;  /* vert outer gap between windows and screen edge */
 static const int smartgaps_fact          = 1;   /* gap factor when there is only one client; 0 = no gaps, 3 = 3x outer gaps */
 static const int showbar                 = 1;   /* 0 means no bar */
 static const int topbar                  = 1;   /* 0 means bottom bar */
